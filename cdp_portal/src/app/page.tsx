9b52522f1c21176c3d7bc4fc8fb3e05c384394f1
Siddharth Verma <siddharthverma1126@gmail.com>
2025-02-15 19:17:44 +0530

    Corrected layout errors

--- cdp_portal/src/app/page.tsx
+++ cdp_portal/src/app/page.tsx
@@ -2,20 +2,5 @@ import Image from "next/image";
 import Navbar from "@/components/navbar";
 
 export default function Home() {
-  return (
-    <div className="fixed top-0 w-full">
-      <Navbar
-        menuItems={[
-          "Home",
-          "About Us",
-          "Notices",
-          "Our Team",
-          "Student Login",
-          "Admin Login",
-          "For Recruiters",
-          "Contact Us",
-        ]}
-      />
-    </div>
-  );
+  return <div>hello</div>;
 }
